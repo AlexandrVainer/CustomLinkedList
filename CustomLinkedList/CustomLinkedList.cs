c011b1e26f224c91c9f133fa663ed1ac3883780b
AlexandrVainer <alexva743@gmail.com>
2022-03-23 21:41:31 +0200

    init

---
+++ CustomLinkedList/CustomLinkedList.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace CustomLinkedList
+{
+    public class CustomLinkedList<T>
+    {
+        public Node<T>? First { get; private set; }
+        public Node<T>? Last;
+        private Node<T>? Current;
+        private Node<T>? prev;
+
+        public CustomLinkedList(){}
+        public void Add(T str)
+        {
+            Last = new Node<T>();
+            Last.NodeValue = str;
+            Last.NextNode = null;
+            Last.PreviousNode = prev;
+
+            if (prev == null)
+                First = Last;
+            else
+                SetNextNodeInBeforeNode(Last);
+
+            prev = Last;
+
+        }
+        private void SetNextNodeInBeforeNode(Node<T> last)
+        {
+            Current = First;
+            while (Current.NextNode != null)
+            {
+                Current = Current.NextNode;
+            }
+
+            Current.NextNode = last;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            Current = First;
+
+            while (Current != null)
+            {
+                yield return Current.NodeValue;
+                Current = Current.NextNode;
+            }
+        }
+
+        public IEnumerable GetEnumerableDESC()
+        {
+            Current = Last;
+
+            while (Current != null)
+            {
+                yield return Current.NodeValue;
+                Current = Current.PreviousNode;
+            }
+        }
+    }
+
+}
---
+++ CustomLinkedList/Node.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomLinkedList
+{
+    public class Node<T>
+    {
+        public Node<T> PreviousNode { get; set; }
+        public Node<T> NextNode { get; set; }
+        public T NodeValue { get; set; }
+
+    }
+
+    public class Node{}
+}
